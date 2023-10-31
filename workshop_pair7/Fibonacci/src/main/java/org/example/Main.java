be83e0a79d92bda1945e87f33c06ed7f4421b29d
Rukiye Arabacı <rukiyee_arabaci@hotmail.com>
2023-10-31 14:56:18 +0300

    algorithm

---
+++ workshop_pair7/Fibonacci/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        long fib1=0;
+        long fib2=1;
+        long total;
+
+        for(int i =1;i<=100;i++) {
+            System.out.print(fib1+" ");
+            total=fib1+fib2;
+            fib1=fib2;
+            fib2=total;
+
+        }
+    }
+}
